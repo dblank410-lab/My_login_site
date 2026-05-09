07fc14c50ee378d5838b14b202889110c22a5f6a
dblank410-lab <dblank410@gmail.com>
2026-05-09 22:54:58 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -7,7 +7,7 @@ form.addEventListener("submit", function(e){
     e.preventDefault();
 
     alert(
-        "Educational demo only.\nNever enter real passwords into unknown websites."
+        "an emial shoulld be sent to me"
     );
 
 });
